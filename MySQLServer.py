22479ebd121a0c1a5e0d30e3a3e5c390659fff01
Riddy23 <redemptionsuwali@gmail.com>
2025-08-15 14:04:34 +0200

    Add files via upload

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,11 +1,10 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
         # Connect to MySQL server
         connection = mysql.connector.connect(
-            host='localhost',       # replace if different
+            host='localhost',       # replace with your host if needed
             user='root',            # replace with your MySQL username
             password=''             # replace with your MySQL password
         )
@@ -16,7 +15,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
