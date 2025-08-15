97098cfa985fe504bf3ea22f48cc4a99e69655d8
Riddy23 <redemptionsuwali@gmail.com>
2025-08-15 14:25:52 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -6,5 +6,5 @@ USE alx_book_store;
 -- ==========================================
 -- Insert a single row into Customers table
 -- ==========================================
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Philani Mkhwananzi', 'philanimk@gmail.com', '123 Happiness Ave.');
