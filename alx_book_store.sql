e94ce0bac6515a8880615eb84701ceb5ca92e055
Riddy23 <redemptionsuwali@gmail.com>
2025-08-15 14:01:18 +0200

    Add files via upload

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,10 +23,7 @@ CREATE TABLE IF NOT EXISTS Books (
     author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    CONSTRAINT fk_books_author FOREIGN KEY (author_id)
-        REFERENCES Authors(author_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 -- ==========================================
@@ -41,16 +38,13 @@ CREATE TABLE IF NOT EXISTS Customers (
 
 -- ==========================================
 -- Table: Orders
--- ==========================================CREATE TABLE IF NOT EXISTS Orders (
+-- ==========================================
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id)
-       REFERENCES Customers(customer_id)
-       
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-
-
 
 -- ==========================================
 -- Table: Order_Details
@@ -60,12 +54,6 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    CONSTRAINT fk_orderdetails_order FOREIGN KEY (order_id)
-        REFERENCES Orders(order_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    CONSTRAINT fk_orderdetails_book FOREIGN KEY (book_id)
-        REFERENCES Books(book_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
