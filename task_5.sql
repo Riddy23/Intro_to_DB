91ee9f3a2df825753a59431518530aa9ae629ddc
Riddy23 <redemptionsuwali@gmail.com>
2025-08-15 14:24:55 +0200

    Add files via upload

--- task_5.sql
+++ task_5.sql
@@ -7,4 +7,4 @@ USE alx_book_store;
 -- Insert a single row into Customers table
 -- ==========================================
 INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (1, 'Philani Mkhwananzi', 'philanimk@gmail.com', '123 Happiness Ave.');
