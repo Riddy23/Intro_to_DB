cc17511ee7319cd16e55eb58da2943de7e23d9ad
Riddy23 <redemptionsuwali@gmail.com>
2025-08-15 14:15:23 +0200

    Add files via upload

---
+++ task_4.sql
@@ -0,0 +1,18 @@
+-- ==========================================
+-- Use the database
+-- ==========================================
+USE alx_book_store;
+
+-- ==========================================
+-- List full description of the table 'Books'
+-- ==========================================
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+  AND TABLE_NAME = 'Books';
